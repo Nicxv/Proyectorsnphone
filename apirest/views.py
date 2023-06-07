037161e5fa211d970d5ec0fd786f6aaeb350e561
Nicxv <nico0valenzuelacl@gmail.com>
2023-06-07 17:23:54 -0400

    apirest

--- apirest/views.py
+++ apirest/views.py
@@ -44,4 +44,53 @@ def lista_venta(request):
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET', 'PUT', 'DELETE'])  
+def lista_u(request, id):
+    """
+    Get, update, o delete de un usuario en particular
+    """
+    try:
+        usu = Usuario.objects.get(id_usuario=id)     
+    except Usuario.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)     
+    if request.method == 'GET':
+        serializer = UsuarioSerializers(usu)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UsuarioSerializers(usu, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        usu.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])  
+def lista_v(request, id):
+    """
+    Get, update, o delete de un usuario en particular
+    """
+    try:
+        ven = Venta.objects.get(id_venta=id)     
+    except Usuario.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)     
+    if request.method == 'GET':
+        serializer = VentaSerializers(ven)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = VentaSerializers(ven, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        ven.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)        
 
